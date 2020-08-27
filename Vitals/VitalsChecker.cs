4b243053b4c68ea5720b70af47c1634750c158a2
Chalasani <chalasanivarsha@gmail.com>
2020-08-27 20:07:19 +0530

    Reduced CCN in CheckVitals

--- Vitals/VitalsChecker.cs
+++ Vitals/VitalsChecker.cs
@@ -7,21 +7,22 @@ namespace Vitals
     {
         public static bool CheckVitals(float bpm, float spo2, float respRate)
         {
-            if (Check_BPM(bpm) && Check_SPO2(spo2) && Check_respRate(respRate))
-            {
-                return true;
-            }
-            return false;
+            bool condition1 = Check_BPM(bpm);
+            bool condition2 = condition1 && Check_SPO2(spo2);
+            bool condition3 = condition2 && Check_respRate(respRate);
+
+            return condition3;
+            
         }
-        public static bool  Check_BPM(float bpm)
+        public static bool Check_BPM(float bpm)
         {
-             return (bpm >= 70 && bpm <= 150);
+            return (bpm >= 70 && bpm <= 150);
         }
         public static bool Check_SPO2(float spo2)
         {
             return (spo2 >= 90);
         }
-        public static bool Check_respRate(float spo2)
+        public static bool Check_respRate(float respRate)
         {
             return (respRate >= 30 && respRate <= 95);
         }
